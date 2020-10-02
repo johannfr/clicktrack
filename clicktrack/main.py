8a51b6f3be4b471a48b402140414924963e5e970
Johann Fridriksson <johann.fridriksson@gmail.com>
2020-10-02 13:24:53 +0200

    🚸 Mostly UX, but its becoming more like I want it to be.

--- clicktrack/main.py
+++ clicktrack/main.py
@@ -1,10 +1,22 @@
 import click
+import sys
 from pydub import AudioSegment
-from pydub.playback import play
+
+
+def float_range(start, stop, step):
+    while start < stop:
+        yield start
+        start += step
+
+
+def info(flag, message):
+    if flag:
+        sys.stdout.write(message)
+        sys.stdout.flush()
 
 
 @click.command()
-@click.option("--bpm", default=120, help="BeatsPerMinute")
+@click.option("--bpm", default=120, help="Beats per minute")
 @click.option(
     "--highclick",
     default="cowbell-high.wav",
@@ -18,7 +30,10 @@ from pydub.playback import play
     help="Path to audio-file for low-pitched click",
 )
 @click.option(
-    "--countprefix", default=4, help="How meany measures to prefix with click"
+    "--countprefix",
+    default=4,
+    help="How meany measures to prefix with click",
+    show_default=True,
 )
 @click.option("--division", default=4, help="Measure division", show_default=True)
 @click.option(
@@ -49,6 +64,14 @@ from pydub.playback import play
     help="Pan for click-track, -1.0 to 1.0",
     show_default=True,
 )
+@click.option(
+    "--track-padding",
+    default=10,
+    type=int,
+    help="Padding before backing-track (ms)",
+    show_default=True,
+)
+@click.option("-v", "--verbose", is_flag=True)
 @click.argument("backingtrack", type=click.Path(exists=True))
 @click.argument("output", type=click.Path())
 def main(
@@ -63,35 +86,45 @@ def main(
     gain_click,
     pan_track,
     pan_click,
+    track_padding,
+    verbose,
 ):
     duration_beat = (60 * 1000) / bpm
+    info(verbose, "Loading click-samples [{} dB]...".format(gain_click))
     click_high = AudioSegment.from_file(highclick) + gain_click
-    if len(click_high) > duration_beat:
-        print(
-            "Warning: High-pitched click sample is {:.1f}ms longer than 1 beat".format(
-                len(click_high) - duration_beat
-            )
-        )
-    click_high_padding = AudioSegment.silent(duration=duration_beat - len(click_high))
     click_low = AudioSegment.from_file(lowclick) + gain_click
-    if len(click_low) > duration_beat:
-        print(
-            "Warning: Low-pitched click sample is {:.1f}ms longer than 1 beat".format(
-                len(click_low) - duration_beat
-            )
-        )
-    click_low_padding = AudioSegment.silent(duration=duration_beat - len(click_low))
-    click_measure = (
-        click_high
-        + click_high_padding
-        + ((click_low + click_low_padding) * (division - 1))
-    ).pan(pan_click)
-    track_padding = AudioSegment.silent(duration=duration_beat * division * countprefix)
+    info(verbose, " Done\n")
+    info(
+        verbose,
+        "Loading and processing backing-track [{}{} dB, {} ms padding]...".format(
+            "+" if gain_track > 0 else "", gain_track, track_padding
+        ),
+    )
+    total_padding = AudioSegment.silent(
+        duration=track_padding + (duration_beat * division * countprefix)
+    )
     track = (
-        track_padding + AudioSegment.from_file(backingtrack).pan(pan_track)
+        total_padding + AudioSegment.from_file(backingtrack).pan(pan_track)
     ) + gain_track
-    mix = track.overlay(click_measure, loop=True)
+    info(verbose, " Done\n")
+
+    # Generate an empty click-track
+    info(verbose, "Generating click-track [{} BPM, {} div]...".format(bpm, division))
+    click_track = AudioSegment.silent(duration=len(track))
+    count = 0
+    for i in float_range(0, len(track), duration_beat):
+        click_track = click_track.overlay(
+            click_high if count % division == 0 else click_low, position=round(i)
+        )
+        count += 1
+    info(verbose, " Done\n")
+
+    info(verbose, "Mixing...")
+    mix = track.overlay(click_track.pan(pan_click))
+    info(verbose, " Done\n")
+    info(verbose, "Exporting to: {}...".format(output))
     mix.export(output, format="mp3")
+    info(verbose, " Done\n")
 
 
 if __name__ == "__main__":
